16ad11f812cdd8a45f363886aa6aed35299a819e
unknown <dawidw22@gmail.com>
2022-09-22 13:55:47 +0200

    feat: create render function

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-const getCardsData = (numberCards) => {
+const getCardsData = (nrCards) => {
   const cards = [];
-  for (let i = 0; i < numberCards; i++) {
+  for (let i = 0; i < nrCards; i++) {
     cards[i] = { id: i, img: `https://robohash.org/${i}?set=set4` };
   }
   const cards2 = cards;
@@ -14,4 +14,22 @@ const randomizeCards = () => {
   return cards;
 };
 
+// Render cards
+const renderCards = (cardsList) => {
+  const root = document.getElementById("root");
+  cardsList.forEach((element) => {
+    root.appendChild(renderSingleCard(element.img, element.id));
+  });
+};
+
+// Create Card Element
+const renderSingleCard = (imgUrl, id) => {
+  const box = document.createElement("div");
+  box.id = id;
+  const imgElement = document.createElement("img");
+  imgElement.src = imgUrl;
+  box.appendChild(imgElement);
+  return box;
+};
 console.log(randomizeCards());
+renderCards(randomizeCards(8));
