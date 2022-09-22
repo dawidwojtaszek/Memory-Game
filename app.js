e61cde947fba71175a56b1f35c561decbba0ef42
unknown <dawidw22@gmail.com>
2022-09-22 13:25:33 +0200

    feat: creating card data

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
+const getCardsData = (numberCards) => {
+  const cards = [];
+  for (let i = 0; i < numberCards; i++) {
+    cards[i] = { id: i, img: `https://robohash.org/${i}?set=set4` };
+  }
+  const cards2 = cards;
+  const mergeCards = cards.concat(cards2);
+  return mergeCards;
+};
 
+const randomizeCards = () => {
+  const cards = getCardsData(8);
+  cards.sort(() => Math.random() - 0.5);
+  return cards;
+};
+
+console.log(randomizeCards());
